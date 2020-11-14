ec84bdd316640678546802c228c8d3fbfd83bbe0
Randall Kwiatkowski <randenko@gmail.com>
2020-11-13 19:03:51 -0500

    add lease agreement as pdf type

---
+++ src/main/java/com/paperstreetsoftware/pdf/domain/LeaseAgreementBean.java
@@ -0,0 +1,8 @@
+package com.paperstreetsoftware.pdf.domain;
+
+import java.io.Serializable;
+
+public class LeaseAgreementBean implements Serializable {
+    private static final long serialVersionUID = 275605412301034177L;
+
+}
--- src/main/java/com/paperstreetsoftware/pdf/model/PDFType.java
+++ src/main/java/com/paperstreetsoftware/pdf/model/PDFType.java
@@ -1,10 +1,12 @@
 package com.paperstreetsoftware.pdf.model;
 
+import com.paperstreetsoftware.pdf.domain.LeaseAgreementBean;
 import com.paperstreetsoftware.pdf.domain.StandardPromissoryNoteBean;
 import com.paperstreetsoftware.pdf.domain.TaxForm1099Bean;
 
 public enum PDFType {
 
+    LEASE_AGREEMENT("lease-agreement_en_US.ftlh", LeaseAgreementBean.class),
     STANDARD_PROMISSORY_NOTE("standard-promissory-note_en_US.ftlh", StandardPromissoryNoteBean.class),
     TAX_FORM_1099("tax-form-1099_en_US.ftlh", TaxForm1099Bean.class);
 
